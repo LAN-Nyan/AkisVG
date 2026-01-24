165794730c80990a00d2952396c9b22fb908955b
Lannyan <safarim@myyahoo.com>
2026-01-24 13:40:16 -0700

    Add QUndoStack support to VectorCanvas constructor

--- src/canvas/vectorcanvas.h
+++ src/canvas/vectorcanvas.h
@@ -3,6 +3,7 @@
 
 #include <QGraphicsScene>
 #include <QGraphicsSceneMouseEvent>
+#include <QUndoStack>
 
 class Project;
 class Tool;
@@ -13,20 +14,21 @@ class VectorCanvas : public QGraphicsScene
     Q_OBJECT
 
 public:
-    explicit VectorCanvas(Project *project, QObject *parent = nullptr);
+    explicit VectorCanvas(Project *project, QUndoStack *undoStack, QObject *parent = nullptr);
     ~VectorCanvas();
-    
+
     Project* project() const { return m_project; }
-    
+    QUndoStack* undoStack() const { return m_undoStack; }
+
     // Tool management
     void setCurrentTool(Tool *tool);
     Tool* currentTool() const { return m_currentTool; }
-    
+
     // Object management
     void addObject(VectorObject *obj);
     void removeObject(VectorObject *obj);
     void clearCurrentFrame();
-    
+
     // Refresh display
     void refreshFrame();
 
@@ -34,12 +36,17 @@ protected:
     void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
     void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
+    void dragEnterEvent(QGraphicsSceneDragDropEvent *event) override;
+    void dragMoveEvent(QGraphicsSceneDragDropEvent *event) override;
+    void dropEvent(QGraphicsSceneDragDropEvent *event) override;
 
 private slots:
+    void setupLayerConnections();
     void onFrameChanged(int frame);
 
 private:
     Project *m_project;
+    QUndoStack *m_undoStack;
     Tool *m_currentTool;
     QGraphicsRectItem *m_canvasBounds;
 };
